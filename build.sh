78b827dffe51f0e761ca1771a7059e1e0286f645
Max Izmailov <me@kvakes.com>
2011-10-22 12:05:54 +0200

    Vera.epub -> vera.epub

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-zip -X Vera.epub mimetype
-zip -rg Vera.epub META-INF/ -x *.DS_Store
-zip -rg Vera.epub OEBPS/ -x *.DS_Store
+zip -X vera.epub mimetype
+zip -rg vera.epub META-INF/ -x *.DS_Store
+zip -rg vera.epub OEBPS/ -x *.DS_Store
