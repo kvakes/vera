86ac4b71d427d995a3806db96c499ba73714df6a
Max Izmailov <me@kvakes.com>
2011-12-02 10:58:48 +0100

    Errata for what-i-believe

--- js/loader.coffee
+++ js/loader.coffee
@@ -1,14 +1,16 @@
 if screen.width < 480
   return
 
+root = 'http://kvakes.github.com/vera/'
+
 window.b = document.getElementsByTagName('body')[0]
 window.i = document.getElementById 'leo'
 
 f = document.createElement 'script'
 s = document.createElement 'script'
 f.type = s.type = 'text/javascript'
-f.src = 'js/flipper.js'
-s.src = 'js/striker.js'
+f.src = root + 'js/flipper.js'
+s.src = root + 'js/striker.js'
 
 b.appendChild f
-#b.appendChild s
+b.appendChild s
--- js/loader.js
+++ js/loader.js
@@ -1,14 +1,16 @@
 (function() {
-  var f, s;
+  var f, root, s;
   if (screen.width < 480) {
     return;
   }
+  root = 'http://kvakes.github.com/vera/';
   window.b = document.getElementsByTagName('body')[0];
   window.i = document.getElementById('leo');
   f = document.createElement('script');
   s = document.createElement('script');
   f.type = s.type = 'text/javascript';
-  f.src = 'js/flipper.js';
-  s.src = 'js/striker.js';
+  f.src = root + 'js/flipper.js';
+  s.src = root + 'js/striker.js';
   b.appendChild(f);
+  b.appendChild(s);
 }).call(this);
